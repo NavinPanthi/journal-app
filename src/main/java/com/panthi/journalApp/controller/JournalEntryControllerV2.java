725c90ab21c350a9ba2c2fde95f9a9edf8d55cf8
NavinPanthi <panthinabin341@gmail.com>
2024-12-26 17:26:48 +0545

    feat: implement http get item by Id, delete, put and patch request

--- src/main/java/com/panthi/journalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/com/panthi/journalApp/controller/JournalEntryControllerV2.java
@@ -2,9 +2,11 @@ package com.panthi.journalApp.controller;
 
 import com.panthi.journalApp.entity.JournalEntry;
 import com.panthi.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,26 +22,49 @@ public class JournalEntryControllerV2 {
     @GetMapping
     public List<JournalEntry> getAll(){
         return journalEntryService.getAll();
+
     }
     @PostMapping
-    public boolean createEntry(@RequestBody JournalEntry myEntry){
+    public JournalEntry createEntry(@RequestBody JournalEntry myEntry){
+        myEntry.setDate(LocalDateTime.now());
         journalEntryService.saveEntry(myEntry);
-        return true;
+        return myEntry;
     }
     @GetMapping("{myId}")
-    public JournalEntry getJournalById(@PathVariable Long myId){
-        return null;
+    public JournalEntry getJournalById(@PathVariable ObjectId myId){
+        return journalEntryService.getById(myId).orElse(null);
     }
     @DeleteMapping("{myId}")
-    public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
+    public JournalEntry deleteJournalEntryById(@PathVariable ObjectId myId){
+        journalEntryService.deleteById(myId);
         return null;
     }
     @PutMapping("{myId}")
-    public JournalEntry updateJournalEntryById(@RequestBody JournalEntry myEntry){
-        return null;
+    public JournalEntry updateJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry){
+        JournalEntry oldEntry = journalEntryService.getById(myId).orElse(null);
+        System.out.println( oldEntry.getTitle());
+        if(oldEntry != null){
+            oldEntry.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().isEmpty() ? newEntry.getTitle(): oldEntry.getTitle());
+            oldEntry.setContent(newEntry.getContent() != null && !newEntry.getContent().isEmpty() ?newEntry.getContent() : oldEntry.getContent());
+        }
+        System.out.println( oldEntry.getTitle());
+        journalEntryService.saveEntry(oldEntry);
+        return oldEntry;
     }
+    //all fields are updated in put
+    // some field are only updated in patch
     @PatchMapping("{myId}")
-    public JournalEntry patchJournalEntryById(@PathVariable Long myId, @RequestBody JournalEntry myEntry){
-       return null;
+    public JournalEntry patchJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry updatedEntry){
+        JournalEntry oldEntry = journalEntryService.getById(myId).orElse(null);
+        if(oldEntry != null){
+            if(updatedEntry.getTitle() != null && !updatedEntry.getTitle().isEmpty()){
+                oldEntry.setTitle(updatedEntry.getTitle());
+            }
+            if(updatedEntry.getContent() != null && !updatedEntry.getContent().isEmpty()){
+                oldEntry.setContent(updatedEntry.getContent());
+            }
+        }
+        journalEntryService.saveEntry(oldEntry);
+       return oldEntry;
     }
 }
--- src/main/java/com/panthi/journalApp/service/JournalEntryService.java
+++ src/main/java/com/panthi/journalApp/service/JournalEntryService.java
@@ -2,10 +2,12 @@ package com.panthi.journalApp.service;
 
 import com.panthi.journalApp.entity.JournalEntry;
 import com.panthi.journalApp.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class JournalEntryService {
@@ -17,5 +19,11 @@ public class JournalEntryService {
     }
     public List<JournalEntry> getAll(){
         return journalEntryRepository.findAll();
+    }
+    public Optional<JournalEntry> getById(ObjectId myId){
+        return journalEntryRepository.findById(myId);
+    }
+    public void deleteById(ObjectId myId){
+        journalEntryRepository.deleteById(myId);
     }
 }
