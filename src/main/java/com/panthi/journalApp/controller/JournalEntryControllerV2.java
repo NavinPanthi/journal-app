c18dd24ec24a69f801b4209e2bcde2604d92d9ea
NavinPanthi <panthinabin341@gmail.com>
2025-01-02 17:33:06 +0545

    feat: implement response entity class in all journal entry http method

--- src/main/java/com/panthi/journalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/com/panthi/journalApp/controller/JournalEntryControllerV2.java
@@ -21,15 +21,22 @@ public class JournalEntryControllerV2 {
     private JournalEntryService journalEntryService;
 
     @GetMapping
-    public List<JournalEntry> getAll(){
-        return journalEntryService.getAll();
-
+    public ResponseEntity<?> getAll(){
+        List<JournalEntry> all = journalEntryService.getAll();
+        if(all != null && !all.isEmpty()){
+        return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     @PostMapping
-    public JournalEntry createEntry(@RequestBody JournalEntry myEntry){
-        myEntry.setDate(LocalDateTime.now());
-        journalEntryService.saveEntry(myEntry);
-        return myEntry;
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry){
+        try{
+            myEntry.setDate(LocalDateTime.now());
+            journalEntryService.saveEntry(myEntry);
+            return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
     @GetMapping("{myId}")
     public ResponseEntity<JournalEntry> getJournalById(@PathVariable ObjectId myId){
@@ -41,24 +48,25 @@ public class JournalEntryControllerV2 {
     }
 
     @DeleteMapping("{myId}")
-    public JournalEntry deleteJournalEntryById(@PathVariable ObjectId myId){
+    public ResponseEntity<JournalEntry> deleteJournalEntryById(@PathVariable ObjectId myId){
         journalEntryService.deleteById(myId);
-        return null;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
     @PutMapping("{myId}")
-    public JournalEntry updateJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry){
+    public ResponseEntity <JournalEntry> updateJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry){
         JournalEntry oldEntry = journalEntryService.getById(myId).orElse(null);
         if(oldEntry != null){
             oldEntry.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().isEmpty() ? newEntry.getTitle(): oldEntry.getTitle());
             oldEntry.setContent(newEntry.getContent() != null && !newEntry.getContent().isEmpty() ?newEntry.getContent() : oldEntry.getContent());
+            journalEntryService.saveEntry(oldEntry);
+            return new ResponseEntity<>(oldEntry, HttpStatus.OK);
         }
-        journalEntryService.saveEntry(oldEntry);
-        return oldEntry;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     //all fields are updated in put
     // some field are only updated in patch
     @PatchMapping("{myId}")
-    public JournalEntry patchJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry updatedEntry){
+    public ResponseEntity<JournalEntry> patchJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry updatedEntry){
         JournalEntry oldEntry = journalEntryService.getById(myId).orElse(null);
         if(oldEntry != null){
             if(updatedEntry.getTitle() != null && !updatedEntry.getTitle().isEmpty()){
@@ -67,8 +75,9 @@ public class JournalEntryControllerV2 {
             if(updatedEntry.getContent() != null && !updatedEntry.getContent().isEmpty()){
                 oldEntry.setContent(updatedEntry.getContent());
             }
+            journalEntryService.saveEntry(oldEntry);
+            return new ResponseEntity<>(oldEntry, HttpStatus.OK);
         }
-        journalEntryService.saveEntry(oldEntry);
-       return oldEntry;
+            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
